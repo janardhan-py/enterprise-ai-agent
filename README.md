6a6fd6bd53ffa9f08f8d02395c3d0e650bf14401
janardhan-py <janardhann1998@gmail.com>
2026-01-29 19:24:49 +0530

    Day 4 : Refactored code and added logging support

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # Enterprise AI Agent
 
 Learning-by-building AI system step by step.
+## Current Features
+- User input handling
+- Persistent memory using JSON
+- Update & reset memory
+- Basic application logging
--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 from core.user_input import get_user_details , ask_user_choice
 from core.memory import load_memory , save_memory, reset_memory
+import logging
+from core.logger import setup_logger
+
+setup_logger()
+
+logging.info("Application started ")
 
 memory = load_memory()
 
 if memory:
+    logging.info("Existing memory found")
     print(f"welcome back, {memory['name']}")
     print(f"last Goal", {memory['goal']})
 
@@ -12,16 +19,20 @@ if memory:
     if choice == '2':
         _, new_goal = get_user_details()
         save_memory(memory["name"], new_goal)
+        logging.info("user goal updated")
         print("Goal updated successfully.")
     
     elif choice == '3':
         reset_memory()
+        logging.info("Memory reset by user")
         print("memory reset successfully.")
 
     else:
+        logging.info("user continue  with exixting goal")
         print("continuw with existing goal ")
 
 else :
+    logging.info("No memory found, asking user input")
     name , goal = get_user_details()
     save_memory(name , goal)
     print(" Your details have been saved")
