2a481b744d129ae472bc295babf6d8d7d00e29ec
janardhan-py <janardhann1998@gmail.com>
2026-01-31 08:37:41 +0530

    Day 5: Fixed rules import and goals analysis

---
+++ core/rules.py
@@ -0,0 +1,14 @@
+
+def analyze_goal(goal: str) -> str:
+    goal = goal.lower()
+
+    if "ai" in goal or "machine learning" in goal:
+        return "You should focus on Python, data structures, and ML basics."
+
+    if "germany" in goal:
+        return "Focus on German language, system design, and EU-compliant AI."
+
+    if "job" in goal or "career" in goal:
+        return "Build real projects, maintain GitHub, and practice interviews."
+
+    return "Break your goal into smaller tasks and work consistently."
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from core.user_input import get_user_details , ask_user_choice
 from core.memory import load_memory , save_memory, reset_memory
 import logging
 from core.logger import setup_logger
+from core.rules import analyze_goal
 
 setup_logger()
 
@@ -14,25 +15,28 @@ if memory:
     print(f"welcome back, {memory['name']}")
     print(f"last Goal", {memory['goal']})
 
+    advice = analyze_goal(memory.get("goal",""))
+
+    print("\nadvice")
+    print(advice)
+
     choice = ask_user_choice()
 
     if choice == '2':
         _, new_goal = get_user_details()
         save_memory(memory["name"], new_goal)
         logging.info("user goal updated")
-        print("Goal updated successfully.")
+        
     
     elif choice == '3':
         reset_memory()
         logging.info("Memory reset by user")
-        print("memory reset successfully.")
-
-    else:
-        logging.info("user continue  with exixting goal")
-        print("continuw with existing goal ")
+        
 
 else :
     logging.info("No memory found, asking user input")
     name , goal = get_user_details()
     save_memory(name , goal)
-    print(" Your details have been saved")
+    advice = analyze_goal(goal)
+    print("\nadvice")
+    print(advice)
